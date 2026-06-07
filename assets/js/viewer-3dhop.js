f274ef3b2dca4ec67fc1e7a35f330f85906f17ec
arise-furg <alexmartire@furg.br>
2026-06-07 18:26:57 -0300

    Update viewer-3dhop.js

--- assets/js/viewer-3dhop.js
+++ assets/js/viewer-3dhop.js
@@ -25,9 +25,9 @@ let anotacoesVisiveis = false;
   const CAMERA_START_THETA = 0.0;
 */
 
-const MODEL_ROTATION_X = 0;
+const MODEL_ROTATION_X = -90;
 const MODEL_ROTATION_Y = 0;
-const MODEL_ROTATION_Z = 0;
+const MODEL_ROTATION_Z = 180;
 
 const CAMERA_START_DISTANCE = 1.15;
 const CAMERA_START_PHI = 0.0;
